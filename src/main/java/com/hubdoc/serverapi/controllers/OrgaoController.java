a99705d3235f7365e2b68d73a44525877bc153db
dede <warder63@gmail.com>
2026-01-28 15:32:35 -0300

    feat orgaosController: find by id

--- src/main/java/com/hubdoc/serverapi/controllers/OrgaoController.java
+++ src/main/java/com/hubdoc/serverapi/controllers/OrgaoController.java
@@ -3,11 +3,13 @@ package com.hubdoc.serverapi.controllers;
 import com.hubdoc.serverapi.dto.DocumentoDTO;
 import com.hubdoc.serverapi.dto.OrgaoDTO;
 import com.hubdoc.serverapi.services.OrgaoService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,12 @@ public class OrgaoController {
 
     @Autowired
     private OrgaoService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<OrgaoDTO> findById(@PathVariable Long id){
+        OrgaoDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
 
     @GetMapping
     public ResponseEntity<Page<OrgaoDTO>> findAll(Pageable pageable){
--- src/main/java/com/hubdoc/serverapi/services/OrgaoService.java
+++ src/main/java/com/hubdoc/serverapi/services/OrgaoService.java
@@ -5,11 +5,14 @@ import com.hubdoc.serverapi.domain.entities.Orgao;
 import com.hubdoc.serverapi.dto.DocumentoDTO;
 import com.hubdoc.serverapi.dto.OrgaoDTO;
 import com.hubdoc.serverapi.repositories.OrgaoRepository;
+import com.hubdoc.serverapi.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Service
 public class OrgaoService {
@@ -17,6 +20,13 @@ public class OrgaoService {
     @Autowired
     private OrgaoRepository repository;
 
+    @Transactional(readOnly = true)
+    public OrgaoDTO findById(Long id) {
+        Orgao orgao = repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Id não encontrado!")
+        );
+        return new OrgaoDTO(orgao);
+    }
     @Transactional(readOnly = true)
     public Page<OrgaoDTO> findAll(Pageable pageable){
         Page<Orgao> result = repository.findAll(pageable);
