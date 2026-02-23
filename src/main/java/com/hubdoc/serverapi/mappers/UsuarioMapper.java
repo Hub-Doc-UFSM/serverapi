891b042107c4431f9cd09488c41bf10d63ac6afc
dede <warder63@gmail.com>
2026-02-23 20:59:58 -0300

    mapper de usuario criado

--- src/main/java/com/hubdoc/serverapi/mappers/UsuarioMapper.java
+++ src/main/java/com/hubdoc/serverapi/mappers/UsuarioMapper.java
@@ -1,4 +1,45 @@
 package com.hubdoc.serverapi.mappers;
 
+import com.hubdoc.serverapi.domain.entities.Usuario;
+import com.hubdoc.serverapi.dto.usuario.UsuarioInputDTO;
+import com.hubdoc.serverapi.dto.usuario.UsuarioResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class UsuarioMapper {
+
+    public Usuario toEntity(UsuarioInputDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+
+        Usuario entity = new Usuario();
+        copyToEntity(dto, entity);
+        return entity;
+    }
+
+    public UsuarioResponseDTO toResponseDTO(Usuario entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        UsuarioResponseDTO dto = new UsuarioResponseDTO();
+        dto.setId(entity.getId());
+        dto.setNome(entity.getNome());
+        dto.setCodigoDeBarras(entity.getCodigoDeBarras());
+        dto.setAtivo(entity.getAtivo());
+        dto.setTipoUsuario(entity.getTipoUsuario());
+
+        return dto;
+    }
+
+    public void copyToEntity(UsuarioInputDTO dto, Usuario entity) {
+        entity.setNome(dto.getNome());
+        entity.setCodigoDeBarras(dto.getCodigoDeBarras());
+        entity.setSenha(dto.getSenha());
+        entity.setAtivo(dto.getAtivo());
+        entity.setTipoUsuario(dto.getTipoUsuario());
+    }
 }
