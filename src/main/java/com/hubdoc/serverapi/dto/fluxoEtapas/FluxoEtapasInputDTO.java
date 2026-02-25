c7f95a68da2d3cf2da2cbf4a432ef8464461c29c
dede <warder63@gmail.com>
2026-02-25 18:03:39 -0300

    Feat 8: Fluxo etapa dtos

--- src/main/java/com/hubdoc/serverapi/dto/fluxoEtapas/FluxoEtapasInputDTO.java
+++ src/main/java/com/hubdoc/serverapi/dto/fluxoEtapas/FluxoEtapasInputDTO.java
@@ -1,4 +1,14 @@
 package com.hubdoc.serverapi.dto.fluxoEtapas;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class FluxoEtapasInputDTO {
+    private Integer ordem;
+    private Long contratoId;
+    private Long etapaId;
 }
--- src/main/java/com/hubdoc/serverapi/dto/fluxoEtapas/FluxoEtapasResponseDTO.java
+++ src/main/java/com/hubdoc/serverapi/dto/fluxoEtapas/FluxoEtapasResponseDTO.java
@@ -1,4 +1,18 @@
 package com.hubdoc.serverapi.dto.fluxoEtapas;
 
+import com.hubdoc.serverapi.dto.contrato.ContratoResponseDTO;
+import com.hubdoc.serverapi.dto.etapa.EtapaResponseDTO;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class FluxoEtapasResponseDTO {
+    @EqualsAndHashCode.Include
+    private Long id;
+    private Integer ordem;
+    private ContratoResponseDTO contrato;
+    private EtapaResponseDTO etapa;
 }
