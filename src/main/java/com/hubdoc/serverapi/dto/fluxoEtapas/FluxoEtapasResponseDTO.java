a81891dfd9bfac6fd2ccc7ee73baeb91eac3b8a9
dede <warder63@gmail.com>
2026-02-25 18:01:27 -0300

    Feat 8: Fluxo etapa repository

---
+++ src/main/java/com/hubdoc/serverapi/dto/fluxoEtapas/FluxoEtapasResponseDTO.java
@@ -0,0 +1,4 @@
+package com.hubdoc.serverapi.dto.fluxoEtapas;
+
+public class FluxoEtapasResponseDTO {
+}
---
+++ src/main/java/com/hubdoc/serverapi/repositories/FluxoEtapasRepository.java
@@ -0,0 +1,7 @@
+package com.hubdoc.serverapi.repositories;
+
+import com.hubdoc.serverapi.domain.entities.FluxoEtapas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FluxoEtapasRepository extends JpaRepository<FluxoEtapas, Long> {
+}
