5f5cb1b87f0e752d241f2821bf7d1c26be413d44
dede <warder63@gmail.com>
2026-02-25 23:09:53 -0300

    Feat 10: Parecer Arquivistico DTOs

--- src/main/java/com/hubdoc/serverapi/dto/parecerArquivistico/ParecerArquivisticoInputDTO.java
+++ src/main/java/com/hubdoc/serverapi/dto/parecerArquivistico/ParecerArquivisticoInputDTO.java
@@ -1,4 +1,31 @@
 package com.hubdoc.serverapi.dto.parecerArquivistico;
 
+import com.hubdoc.serverapi.domain.enums.EstadoConservacao;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class ParecerArquivisticoInputDTO {
+
+    private LocalDateTime dataParecer;
+    private Boolean rasgado;
+    private Boolean semPerdaInfo;
+    private Boolean manchas;
+    private Boolean comPerdaInfo;
+    private Boolean danoFerrugem;
+    private Boolean paginasFaltando;
+    private Boolean acidificado;
+    private Boolean paginasColadas;
+    private Boolean porosoQuebradico;
+    private EstadoConservacao estadoConservacao;
+    private String indicacaoReparacao;
+    private String observacoesGerais;
+    private Long documentoId;
+    private Long usuarioId;
+
 }
--- src/main/java/com/hubdoc/serverapi/dto/parecerArquivistico/ParecerArquivisticoResponseDTO.java
+++ src/main/java/com/hubdoc/serverapi/dto/parecerArquivistico/ParecerArquivisticoResponseDTO.java
@@ -1,4 +1,35 @@
 package com.hubdoc.serverapi.dto.parecerArquivistico;
 
+import com.hubdoc.serverapi.domain.enums.EstadoConservacao;
+import com.hubdoc.serverapi.dto.documento.DocumentoResponseDTO;
+import com.hubdoc.serverapi.dto.usuario.UsuarioResponseDTO;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class ParecerArquivisticoResponseDTO {
+
+    @EqualsAndHashCode.Include
+    private Long id;
+    private LocalDateTime dataParecer;
+    private Boolean rasgado;
+    private Boolean semPerdaInfo;
+    private Boolean manchas;
+    private Boolean comPerdaInfo;
+    private Boolean danoFerrugem;
+    private Boolean paginasFaltando;
+    private Boolean acidificado;
+    private Boolean paginasColadas;
+    private Boolean porosoQuebradico;
+    private EstadoConservacao estadoConservacao;
+    private String indicacaoReparacao;
+    private String observacoesGerais;
+    private DocumentoResponseDTO documento;
+    private UsuarioResponseDTO usuario;
+
 }
