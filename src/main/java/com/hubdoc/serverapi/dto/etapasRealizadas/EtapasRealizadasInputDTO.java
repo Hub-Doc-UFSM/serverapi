c72f65856f056c7b2a6b479a5a85b3dc019fc53d
dede <warder63@gmail.com>
2026-02-25 22:25:13 -0300

    Feat 9 : Etapas Realizadas Dtos

--- src/main/java/com/hubdoc/serverapi/dto/etapasRealizadas/EtapasRealizadasInputDTO.java
+++ src/main/java/com/hubdoc/serverapi/dto/etapasRealizadas/EtapasRealizadasInputDTO.java
@@ -1,4 +1,24 @@
 package com.hubdoc.serverapi.dto.etapasRealizadas;
 
+import com.hubdoc.serverapi.domain.enums.StatusExecucao;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EtapasRealizadasInputDTO {
+
+    private LocalDateTime dataInicio;
+    private LocalDateTime dataFim;
+    private String observacaoTecnica;
+    private StatusExecucao status;
+    private Long documentoId;
+    private Long etapaId;
+    private Long usuarioId;
 }
--- src/main/java/com/hubdoc/serverapi/dto/etapasRealizadas/EtapasRealizadasResponseDTO.java
+++ src/main/java/com/hubdoc/serverapi/dto/etapasRealizadas/EtapasRealizadasResponseDTO.java
@@ -1,4 +1,27 @@
 package com.hubdoc.serverapi.dto.etapasRealizadas;
 
+import com.hubdoc.serverapi.domain.enums.StatusExecucao;
+import com.hubdoc.serverapi.dto.documento.DocumentoResponseDTO;
+import com.hubdoc.serverapi.dto.etapa.EtapaResponseDTO;
+import com.hubdoc.serverapi.dto.usuario.UsuarioResponseDTO;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class EtapasRealizadasResponseDTO {
+
+    @EqualsAndHashCode.Include
+    private Long id;
+    private LocalDateTime dataInicio;
+    private LocalDateTime dataFim;
+    private String observacaoTecnica;
+    private StatusExecucao status;
+    private DocumentoResponseDTO documento;
+    private EtapaResponseDTO etapa;
+    private UsuarioResponseDTO usuario;
 }
